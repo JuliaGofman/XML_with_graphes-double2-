9afdadcf20ca7a059163520c4314e1c22423aaef
JuliaGofman <35265346+JuliaGofman@users.noreply.github.com>
2018-08-01 20:56:26 +0500

    XML parsing
    with recurcion on tree of tags

---
+++ pyramid_of_cubes.py
@@ -0,0 +1,28 @@
+from xml.etree import ElementTree
+
+# with open('example.xml', 'w') as f:
+#     f.write(input())
+def recurcionForCubes(el, root, value):
+    value += 1
+    dict_value[el.attrib['color']] += [value]
+    for child in el:
+        if child in el:
+            recurcionForCubes(child, el, value)
+
+tree = ElementTree.parse('cubes.xml')
+root = tree.getroot()
+dict_value = {'red':[0], 'green':[0], 'blue':[0]}
+# for el in root.iter('cube'):
+#     if el.attrib['color'] == 'red':
+#         val_red += 1
+#     if el.attrib['color'] == 'green':
+#         val_green += 1
+#     if el.attrib['color'] == 'blue':
+#         val_blue += 1
+
+for el in root:
+    value = 0
+    recurcionForCubes(el, root, value)
+print('{} {} {}'.format(dict_value['red'], dict_value['green'], dict_value['blue']))
+
+# tree.write('cubes.xml')
